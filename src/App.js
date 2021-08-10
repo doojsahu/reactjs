033fdb122f571b5bf01c463b90dd7e41cd7b56e6
DOOJ SAHU <doojsahu@DOOJs-MacBook-Pro.local>
2021-08-10 18:11:15 +0530

    useState-only for funcational used-  counter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
+import react, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [count, setCounter] = useState(0);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div>
+              <div>counter: {count}</div>
+              <button onClick= {() => {
+                setCounter(count + 1);
+              }}>Plus</button>
+              <button onClick = {() => {
+                setCounter(count - 1);
+              }}>Minus</button>
+          </div>
       </header>
     </div>
   );
